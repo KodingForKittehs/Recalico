49a3818ff00d1f6be256c10bd2be51932e7eeba9
KodingForKittehs <kodingforkittehs@gmail.com>
2022-08-08 20:48:59 -0400

    MongoDB Atlas testing

--- db_conn.sh
+++ db_conn.sh
@@ -1,1 +1,5 @@
-docker --rm -it
+#!/bin/bash
+
+DB_URL="mongodb+srv://kodingforkittehs:${RECALLICO_PASS}@recallico.enfadfj.mongodb.net/admin?retryWrites=true&w=majority"
+echo $DB_URL
+docker run --rm -it --network=host mongo mongo "$DB_URL"
